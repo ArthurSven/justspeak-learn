b3cfcf35cb61f133bb76ce61d7bde2995c2e47f3
Arthur Sven Msiska <arthursvenmsiska@gmail.com>
2024-05-31 17:09:27 +0200

    Initial commit, building this on angular frontend and springboot backend

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './components/error/error.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { NgModule } from '@angular/core';
+import { AboutComponent } from './components/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', component: ErrorComponent },
+  //No routes after this point
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
+
+
